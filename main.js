d74d34bd8ed157977eb5d73ebc98a147fe4815d4
Julián Swerdlin <julianswer36@gmail.com>
2021-05-05 14:55:24 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ const atras = document.getElementById("atras");
 const adelante = document.getElementById("adelante");
 
 atras.addEventListener("click", (e) => {
-    e.preventDefault
+    e.preventDefault()
 })
 
 adelante.addEventListener("click", (e) => {
-    e.preventDefault
+    e.preventDefault()
 })
 
 let indice = 1;
@@ -105,4 +105,48 @@ $('#enlace-contacto2').on('click', function(e){
     $('html, body').animate({
         scrollTop: 3250
     }, 600);
+});
+
+const categorias = document.querySelectorAll('#categorias .categoria');
+const contenedorPreguntas = document.querySelectorAll('.contenedor-preguntas');
+let categoriaActiva = null;
+
+categorias.forEach((categoria) => {
+    categoria.addEventListener('click', (e) => {
+        categorias.forEach((elemento) => {
+            elemento.classList.remove('activa');
+        });
+        e.currentTarget.classList.toggle('activa');
+        categoriaActiva = categoria.dataset.categoria;
+        
+        contenedorPreguntas.forEach((contenedor) => {
+            if(contenedor.dataset.categoria === categoriaActiva){
+                contenedor.classList.add('activo')
+            } else {
+                contenedor.classList.remove('activo')
+            }
+        });
+    });
+});
+
+const preguntas = document.querySelectorAll('.preguntas .contenedor-pregunta');
+preguntas.forEach((pregunta) => {
+    pregunta.addEventListener('click', (e) => {
+        e.currentTarget.classList.toggle('activa');
+        const respuesta = pregunta.querySelector('.respuesta');
+		const alturaRealRespuesta = respuesta.scrollHeight;	
+		if(!respuesta.style.maxHeight){
+			respuesta.style.maxHeight = alturaRealRespuesta + 'px';
+		} else {
+			respuesta.style.maxHeight = null;
+        }
+        
+        //opcional (hace que no se puedan abrir 2 preguntas al mismo tiempo)
+        preguntas.forEach((elemento) => {
+            if(pregunta !== elemento){
+            elemento.classList.remove('activa');
+            elemento.querySelector('.respuesta').style.maxHeight = null;
+        }
+        });
+    });
 });
